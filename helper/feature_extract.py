d0cc9b5bac58b7e57656b3630b9213bb95c3d9c6
Simon_Staehli <simon.staehli@students.fhnw.ch>
2021-11-24 14:05:39 +0100

    feature_extract.py adapted with two classes to extract features from raw data

--- helper/feature_extract.py
+++ helper/feature_extract.py
@@ -3,106 +3,213 @@ import numpy as np
 import librosa
 import warnings
 warnings.filterwarnings("ignore")
-from multiprocess import Pool
+from multiprocessing import Pool
+from datetime import datetime
 
-def multiprocessor_wrapper(func_, iterable_: list, processors=3):
+
+class BaseFeatureTransform(object):
+
+    def __init__(self):
+        super(BaseFeatureTransform, self).__init__()
+
+    def transform_extracted_features(self, df: pd.DataFrame):
+        """
+        Transforms given feature Dataframe to consistent shape for given Data by WSL.
+
+        This Dataframe needs the columns: start_time, test_no, sensor_type
+
+        start_time	        | test_no	| sensor_type	|   Features...
+        2021-06-22 15:34:38	|    1	    |   S01	        |
+        2021-06-22 15:34:38	|    1	    |   M01	        |
+        2021-06-22 15:34:38	|    1	    |   G02	        |
+
+        Parameters
+        ----------
+        df: pd.DataFrame
+            Dataframe with all extracted feature records init
+
+        Returns
+        -------
+        df: pd.DataFrame
+            Transformed dataframe
+        """
+        df = df.groupby(['start_time', 'packnr', 'sensor_type']).mean().unstack()
+        df.columns = df.columns.get_level_values(0) + '_' + df.columns.get_level_values(1)
+        df = df.reset_index(level=[0, 1])
+        df['start_time'] = pd.to_datetime(df['start_time'])
+        return df.sort_values(by='packnr', ascending=True)
+
+    def feature_join(self, data_to_join: pd.DataFrame, data_extracted: pd.DataFrame):
+        """
+        Joins initial Dataframe with pre-processed data of WSL with our extracted feature Dataframe.
+
+        Parameters
+        ----------
+        data_to_join: pd.DataFrame
+            Preprocessed dataframe from wsl
+
+        data_extracted: pd.DataFrame
+            Extracted feature dataframe
+
+        Returns
+        -------
+        data_extracted: pd.DataFrame
+            Joined Dataframe
+        """
+        # Data Type Conversion for Join
+        data_to_join['start_time'] = data_to_join['start_time'].astype(str)
+        data_extracted['start_time'] = data_extracted['start_time'].astype(str)
+        data_to_join['packnr'] = data_to_join['packnr'].astype(int)
+        data_extracted['packnr'] = data_extracted['packnr'].astype(int)
+        # Join
+        data_extracted = pd.merge(left=data_to_join, right=data_extracted, how='left',
+                                  left_on=['start_time', 'packnr'], right_on=['start_time', 'packnr'])
+        # Type conversion back to datetime
+        data_extracted['start_time'] = pd.to_datetime(data_extracted['start_time'])
+        return data_extracted
+
+
+class SignalFeatureExtractor(BaseFeatureTransform):
     """
-    Wrapper function for data reading from vlnd to have access to multiple processes on
-    computer. Processing it on multiple processes is much faster than processing it on only one.
-
-    Parameters
-    ----------
-    func_: function-object
-        Function used for multiprocessing. SHould be constructed to process single element like
-        one list element
-
-    iterable_: list
-        List of elements to map to the func_.
-
-    processors: int
-        Number of processes to use.
-
-    Returns
-    -------
-    res: pd.DataFrame
-        Data converted to a pandas.DataFrame
-    """
-
-    # Process data
-    with Pool(processes=processors) as pool:
-        res = pool.map(func=func_, iterable=iterable_)
-
-    return pd.DataFrame.from_records(res)
-
-
-def transform_extracted_features(df: pd.DataFrame):
-    """
-    Transforms given feature Dataframe to consistent shape for given Data by WSL.
-
-    This Dataframe needs the columns: start_time, test_no, sensor_type
-
-    start_time	        | test_no	| sensor_type	|   Features...
-    2021-06-22 15:34:38	|    1	    |   S01	        |
-    2021-06-22 15:34:38	|    1	    |   M01	        |
-    2021-06-22 15:34:38	|    1	    |   G02	        |
-
-    Parameters
-    ----------
-    df: pd.DataFrame
-        Dataframe with all extracted feature records init
-
-    Returns
-    -------
 
     """
-    df = df.groupby(['start_time', 'test_no', 'sensor_type']).mean().unstack()
-    df.columns = df.columns.get_level_values(0) + '_' + df.columns.get_level_values(1)
-    df = df.reset_index(level=[0, 1])
 
-    return df
+    def __init__(self, raw_data_path: str, extract_chromafeatures: True,
+                 n_processes: int = 3):
+        """
+
+        Parameters
+        ----------
+        raw_data_path
+        extract_chromafeatures
+        n_processes
+
+        """
+        super(SignalFeatureExtractor, self).__init__()
+        self.raw_data_path = raw_data_path
+        self.extract_chromafeatures = extract_chromafeatures
+        self.n_processes = n_processes
+
+        print('SignalFeatureExtractor: ', self.__dict__)
+
+    @staticmethod
+    def load_raw_as_list(fp: str):
+        """
+        Reads the raw data file and returns it.
+
+        Parameters
+        ----------
+        fp: str
+            File path to raw data file
+
+        Returns
+        -------
+        raw_data: list
+            list of raw data measurments
+        """
+        raw_data = []
+        with open(fp, 'r') as text_file:
+            for line in text_file:
+                raw_data.append(line)
+        return raw_data
 
 
-def mp_extract_chroma_features(sample: str):
-    """
-    Extracts Chromagraph features from data. This function was written to correspond with the
-    multiprocessing module.
+    def extract(self, processed_data: pd.DataFrame):
+        """
+        Extracts new features and joins it to the given pre-processed dataframe by WSL.
 
-    Parameters
-    ----------
-    sample: list
-        One Sample as Listelement
+        Parameters
+        ----------
+        processed_data: pd.DataFrame
+            Already processed Dataframe by WSL
 
-    Returns
-    -------
-    data_dict: dict
-        Returns a Dictionary with given samples
-    """
-    sample = sample.split(' ')
+        Returns
+        -------
+        data: pd.DataFrame
+            Dataframe with concatenated new extracted features
+        """
+        raw_data = self.load_raw_as_list(fp=self.raw_data_path)
 
-    # Feature Extraction
-    ## Start time
-    start_time = sample[0] + ' ' + sample[1]
-    ## Test number
-    test_no = sample[2]
-    ## Sensor type
-    sensor = sample[3]
-    ## Measurements
-    package = np.array(sample[4:]).astype(float)
-    ## Length of a package
-    package_len = len(package)
-    # Feature Extraction Chroma stft
-    chroma_stft = librosa.feature.chroma_stft(y=package, sr=10000).flatten()
-    chroma_stft_mean = np.mean(chroma_stft)
-    chroma_stft_med = np.median(chroma_stft)
-    chroma_stft_std = np.std(chroma_stft)
+        if self.extract_chromafeatures:
+            print(f'INFO || {datetime.now().strftime("%y.%m.%d_%H:%M")} | Extracting ChromaFeatures from Raw')
+            data = SignalFeatureExtractor._multiprocessor_wrapper(func_=SignalFeatureExtractor.mp_extract_chroma_features,
+                                                                  iterable_=raw_data,
+                                                                  processors=self.n_processes)
+            print(f'INFO || {datetime.now().strftime("%y.%m.%d_%H:%M")} | Transform ChromaFeatures')
+            data = self.transform_extracted_features(df=data)
 
-    # Feature Extraction
-    data_dict = dict(start_time=start_time, test_no=test_no, sensor_type=sensor, package_len=package_len,
-                     chroma_stft_mean=chroma_stft_mean, chroma_stft_med=chroma_stft_med,
-                     chroma_stft_std=chroma_stft_std)
+            print(f'INFO || {datetime.now().strftime("%y.%m.%d_%H:%M")} | Joining ChromaFeatures')
+            data = self.feature_join(data_to_join=processed_data, data_extracted=data)
 
-    return data_dict
+        return data
 
+
+    @staticmethod
+    def _multiprocessor_wrapper(func_, iterable_: list, processors=3):
+        """
+        Wrapper function for data reading from vlnd to have access to multiple processes on
+        computer. Processing it on multiple processes is much faster than processing it on only one.
+
+        Parameters
+        ----------
+        func_: function-object
+            Function used for multiprocessing. SHould be constructed to process single element like
+            one list element
+
+        iterable_: list
+            List of elements to map to the func_.
+
+        processors: int
+            Number of processes to use.
+
+        Returns
+        -------
+        res: pd.DataFrame
+            Data converted to a pandas.DataFrame
+        """
+        # Process data
+        with Pool(processes=processors) as pool:
+            res = pool.map(func=func_, iterable=iterable_)
+        return pd.DataFrame.from_records(res)
+
+
+    def mp_extract_chroma_features(sample: str):
+        """
+        Extracts Chromagraph features from data. This function was written to correspond with the
+        multiprocessing module.
+
+        Parameters
+        ----------
+        sample: list
+            One Sample as List element of list of strings
+
+        Returns
+        -------
+        data_dict: dict
+            Returns a Dictionary with given samples
+        """
+        # generate List from string
+        sample = sample.split(' ')
+
+        # Feature Extraction from list of strings
+        start_time = sample[0] + ' ' + sample[1]
+        packnr = sample[2]
+        sensor = sample[3]
+        package = np.array(sample[4:]).astype(float)
+        package_len = len(package)
+        chroma_stft = librosa.feature.chroma_stft(y=package, sr=10000).flatten()
+        chroma_stft_mean = np.mean(chroma_stft)
+        chroma_stft_med = np.median(chroma_stft)
+        chroma_stft_std = np.std(chroma_stft)
+
+        # Create Dictionary
+        data_dict = dict(start_time=start_time, packnr=packnr, sensor_type=sensor, package_len=package_len,
+                         chroma_stft_mean=chroma_stft_mean, chroma_stft_med=chroma_stft_med,
+                         chroma_stft_std=chroma_stft_std)
+        return data_dict
+
+
+# Standalone Functions below here
 
 def extract_highest_amplitude_features_with_mp(df: pd.DataFrame, sensor_types: list, 
                                                create_one_sensor_feature=True, n_processes=4,
